8322c388b19a2af777f321bafa00cbb47454f43d
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-11 17:59:44 +0100

    added card container.vue

---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,50 @@
+<script>
+import { store } from '../store';
+import NumberOfCards from './NumberOfCards.vue';
+import Cards from './Cards.vue';
+export default {
+    name: 'cardsContainer',
+
+    components: {
+        NumberOfCards,
+        Cards,
+    },
+
+    data() {
+        return {
+            store,
+        };
+    },
+}
+
+</script>
+
+<template>
+    <div class="main-container">
+        <NumberOfCards :found="this.store.itemCard" />
+        <div class="cards-container">
+            <Cards v-for="cards in this.store.itemCard" :img="cards.card_images[0].image_url" :name="cards.name"
+                :type="cards.archetype" />
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use'../../style/partial/_variables.scss' as *;
+
+.main-container {
+    background-color: $col-white;
+    width: 85%;
+    padding: 48px;
+    margin: 0 auto;
+
+    .cards-container {
+        height: 90%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 25px;
+    }
+}
+</style>
